84e6cd6f4de00d4599fa5f3b085e315dbb88484b
Vitalie Bocicov <vitalie.bocicov99@gmail.com>
2024-09-13 12:53:23 +0200

    add task feature implemented

---
+++ task-tracker-cli/src/main/java/util/TaskConverter.java
@@ -0,0 +1,17 @@
+package util;
+
+import org.json.JSONObject;
+import model.Task;
+
+public class TaskConverter {
+    public static JSONObject taskToJson(Task task) {
+        JSONObject json = new JSONObject();
+        json.put("id", task.getId());
+        json.put("description", task.getDescription());
+        json.put("status", task.getStatus().toString());
+        json.put("createdAt", task.getCreatedAt().toString());
+        json.put("updatedAt", task.getUpdatedAt() == null? "null" :task.getUpdatedAt().toString());
+        return json;
+    }
+
+}
