c28bed08d4a21373e481720dd099640cdc3712c7
Vitalie Bocicov <vitalie.bocicov99@gmail.com>
2025-01-19 20:22:30 +0200

    .idea file deleted

--- task-tracker-cli/src/main/java/service/TaskService.java
+++ task-tracker-cli/src/main/java/service/TaskService.java
@@ -1,122 +1,32 @@
 package service;
 
-import model.Status;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import storage.JsonUtil;
 import model.Task;
-import util.TaskConverter;
+import storage.JsonUtil;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class TaskService {
     private static TaskService taskService;
     private final JsonUtil jsonUtil;
+    List<Task> tasks = new ArrayList<>();
 
-    private TaskService() {
-        jsonUtil = JsonUtil.getInstance();
+    private TaskService(JsonUtil jsonUtil){
+        this.jsonUtil = jsonUtil;
     }
 
     public static TaskService getInstance() {
         if (taskService == null) {
-            taskService = new TaskService();
+            taskService = new TaskService(new JsonUtil());
         }
         return taskService;
     }
 
-    public void addTask(String description) {
-        JSONObject jsonObject = jsonUtil.getContent();
-        if (jsonObject == null) {
-            return;
-        }
-
-        JSONArray tasks = jsonObject.getJSONArray("tasks");
-        Task task = new Task(tasks.length() + 1, description);
-        JSONObject taskJson = TaskConverter.taskToJson(task);
-        tasks.put(taskJson);
-        jsonObject.put("tasks", tasks);
-        jsonUtil.writeJsonObject(jsonObject);
+    public void add(String description) {
+        Task task = new Task(1, description);
+        jsonUtil.saveTask(task);
     }
 
-    public void deleteTask(int id) {
-
-        JSONObject jsonObject = jsonUtil.getContent();
-        if (jsonObject == null) {
-            return;
-        }
-
-        JSONArray tasks = jsonObject.getJSONArray("tasks");
-
-        for (int i = 0; i < tasks.length(); i++) {
-            JSONObject task = tasks.getJSONObject(i);
-            if (task.getInt("id") == id) {
-                tasks.remove(i);
-                break;
-            }
-        }
-
-        jsonObject.put("tasks", tasks);
-        jsonUtil.writeJsonObject(jsonObject);
-    }
-
-    public void updateTask(int id, String description) {
-        JSONObject jsonObject = jsonUtil.getContent();
-        if (jsonObject == null) {
-            return;
-        }
-
-        JSONArray tasks = jsonObject.getJSONArray("tasks");
-
-        for (int i = 0; i < tasks.length(); i++) {
-            JSONObject task = tasks.getJSONObject(i);
-            if (task.getInt("id") == id) {
-                task.put("description", description);
-                task.put("updatedAt", LocalDateTime.now());
-                break;
-            }
-        }
-
-        jsonObject.put("tasks", tasks);
-        jsonUtil.writeJsonObject(jsonObject);
-    }
-    public void listAllTasks() {
-        JSONObject jsonObject = jsonUtil.getContent();
-        if (jsonObject == null) {
-            return;
-        }
-
-        JSONArray jsonTasks = jsonObject.getJSONArray("tasks");
-        List<Task> tasks = new ArrayList<>();
-
-        for (int i = 0; i < jsonTasks.length(); i++) {
-            JSONObject json = jsonTasks.getJSONObject(i);
-            tasks.add(TaskConverter.jsonToTask(json));
-        }
-
-        tasks.forEach(System.out::println);
-    }
-
-    public void markTask(int id, Status status) {
-        JSONObject jsonObject = jsonUtil.getContent();
-        if (jsonObject == null) {
-            return;
-        }
-
-        JSONArray tasks = jsonObject.getJSONArray("tasks");
-
-        for (int i = 0; i < tasks.length(); i++) {
-            JSONObject task = tasks.getJSONObject(i);
-            if (task.getInt("id") == id) {
-                task.put("status", status.toString());
-                task.put("updatedAt", LocalDateTime.now());
-                break;
-            }
-        }
-
-        jsonObject.put("tasks", tasks);
-        jsonUtil.writeJsonObject(jsonObject);
+    public void list() {
     }
 }
